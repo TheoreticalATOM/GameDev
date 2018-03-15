3b7d5b7f3351dc2e6e38d200195a5ebdea0dec32
Unknown <duke.jp.freitas@gmail.com>
2018-03-15 13:43:34 +0000

    Vinyl Player

--- Projects/TrueManShow/Assets/Scripts/Interactable/Interactive/ItemPhysicsInteractRaycast.cs
+++ Projects/TrueManShow/Assets/Scripts/Interactable/Interactive/ItemPhysicsInteractRaycast.cs
@@ -5,11 +5,17 @@ public class ItemPhysicsInteractRaycast : ItemPhysicsInteract
     public LayerMask ItemInventoryMask;
     public float dist;
     private RaycastHit hit;
-
+    private Collider CurrentHit;
     protected override bool OnInteract(GameObject player)
     {
-        if (Physics.Raycast(transform.position, Camera.main.transform.forward, out hit, dist, ItemInventoryMask))
+
+        if (Physics.Raycast(transform.position, Camera.main.transform.forward, out hit, dist, ItemInventoryMask) && CurrentHit != hit.collider)
+        {
+            CurrentHit = hit.collider;
+
             return !TryAddToInventory(hit.collider);
+        }
+        CurrentHit = null;
         return true;
     }
 }
