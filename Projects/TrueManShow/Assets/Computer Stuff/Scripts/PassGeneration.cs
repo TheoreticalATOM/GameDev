6f36205a9788384e8bc97729131f80f9b2630d14
Unknown <suuunly@gmail.com>
2018-05-09 21:59:08 +0100

    Day 3 - Daytime/Nightime
    Finished Day 3 (Daycycle), Finished all base Nightcycle, just missing the night time objective.
    (Pedro did his gucci stuff with the cmd ~hacky-boi~, so we have a hacking "minigame" to access the email).
    
    Moving on to Day 4 after/(or)/before adding the day 3 nighttime objectives

---
+++ Projects/TrueManShow/Assets/Computer Stuff/Scripts/PassGeneration.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PassGeneration : MonoBehaviour {
+
+    private List<string> words = new List<string> {"seal","mouse" ,"password", "gravy", "julia"};
+    private string passEnc;
+    public string pass;
+    public GameObject Loginbutton;
+
+    // Use this for initialization
+    void Start () {
+        pass = words[Random.Range(0,4)];
+        char[] passarr= pass.ToCharArray();
+        passEnc = GenerateEncriptedPass(passarr);
+        this.GetComponent<TextAppear>().desiredText = passEnc;
+        Loginbutton.GetComponent<LogIn>().desiredPass = pass;
+
+    }
+
+    public string GenerateEncriptedPass(char[] pass)
+    {
+        for(int i = 0; i<pass.Length; i++)
+        {
+            char tmp = pass[i];
+            int r = Random.Range(i,pass.Length);
+            pass[i] = pass[r];
+            pass[r] = tmp;
+        }
+        return new string(pass);
+    }
+
+    private bool IsInArray(int numb, int[] array)
+    {
+        for (int i = 0; i < array.Length; i++)
+        {
+            if (numb == array[i])
+                return true;
+        }
+        return false;
+    }
+
+    // Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Projects/TrueManShow/Assets/Computer Stuff/Scripts/TextAppear.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TextAppear : MonoBehaviour {
+
+    [TextArea (1,5)]
+    public string desiredText;
+    private string currtext;
+    public GameObject NextText;
+
+	// Use this for initialization
+	void Start () {
+        currtext = GetComponent<Text>().text;
+	}
+
+    public void StartTextAppear()
+    {
+        StartCoroutine(AnimateText(desiredText));
+
+    }
+
+    public void StopTextAppear()
+    {
+        if(NextText != null)
+            NextText.GetComponent<TextAppear>().StartTextAppear();
+    }
+
+    IEnumerator AnimateText(string strComplete)
+    {
+        int i = 0;
+        currtext = "";
+        while (i < strComplete.Length)
+        {
+            currtext += strComplete[i++];
+            GetComponent<Text>().text = currtext;
+            yield return new WaitForSeconds(0.05F);
+        }
+        StopTextAppear();
+    }
+    
+    public void ResetText()
+    {
+        GetComponent<Text>().text = "";
+    }
+
+    // Update is called once per frame
+    void Update () {
+
+    }
+}
