1c8a7a6fd8a15e3cad4f72395cd647cd359aecd5
Unknown <suuunly@gmail.com>
2018-05-10 19:39:58 +0100

    Finished Day 3 and Working on Day 4
    WIP

--- Projects/TrueManShow/Assets/Scripts/Utility/AnimationCompletedTrigger.cs
+++ Projects/TrueManShow/Assets/Scripts/Utility/AnimationCompletedTrigger.cs
@@ -4,9 +4,25 @@ using UnityEngine.Events;
 public class AnimationCompletedTrigger : MonoBehaviour
 {
     public UnityEvent OnAnimationComplete;
+    public UnityEvent[] OnKeyEventsTriggered;
 
+    private int mKeyIndex = 0;
+    
     public void RegisterCompletion()
     {
         OnAnimationComplete.Invoke();
+    }
+
+    public void RegisterKeyEvent()
+    {
+        if (mKeyIndex >= OnKeyEventsTriggered.Length)
+            return;
+        
+        OnKeyEventsTriggered[mKeyIndex++].Invoke();
+    }
+
+    public void ResetKeyEvents()
+    {
+        mKeyIndex = 0;
     }
 }
